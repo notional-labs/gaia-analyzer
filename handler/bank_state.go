ba0808e99b6a0d48b9df1ed362abbeabbd64232e
vuong <nguyenvuong1122000@gmail.com>
2022-04-17 20:09:45 +0700

    test

--- handler/bank_state.go
+++ handler/bank_state.go
@@ -3,10 +3,9 @@ package handler
 import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/notional-labs/gaia-analyzer/db-query/app"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 func ExportStateBankAtHeight(height int64) banktypes.GenesisState {
-	ctx := app.EmptyApp.NewContext(true, tmproto.Header{Height: app.EmptyApp.LastBlockHeight()})
+	ctx := app.GetQueryContext(height)
 	return *app.EmptyApp.BankKeeper.ExportGenesis(ctx)
 }
